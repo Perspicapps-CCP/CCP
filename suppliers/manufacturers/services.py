2a53b62df0893f7dcaf3589771a308f8e664c7f2
andresalarcon <w.alarconc@uniandes.edu.co>
2025-03-28 16:10:40 -0500

    feat: Se crea servicio para la creacion de fabricantes

--- suppliers/manufacturers/services.py
+++ suppliers/manufacturers/services.py
@@ -7,4 +7,23 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
+def create_manufacturer(
+    db: Session, manufacturer: schemas.ManufacturerCreateSchema
+) -> models.Manufacturer:
+    """Create a new manufacturer in the database."""
+    db_manufacturer = models.Manufacturer(
+        id=uuid4(),
+        name=manufacturer.manufacturer_name,
+        identification_type=models.IdentificationType(manufacturer.identification_type),
+        identification_number=manufacturer.identification_number,
+        address=manufacturer.address,
+        contact_phone=manufacturer.contact_phone,
+        email=manufacturer.email
+    )
+    db.add(db_manufacturer)
+    db.flush()
+    db.commit()
+    db.refresh(db_manufacturer)
+    return db_manufacturer
+
 
